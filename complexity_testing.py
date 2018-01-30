4dbffa167d0a4be7c5f41d8b15857a7cd828e124
sansona <jiaming.justin.chen@gmail.com>
2018-01-29 19:39:21 -0800

    Implemented graphing function

--- complexity_testing.py
+++ complexity_testing.py
@@ -1,28 +1,46 @@
 from union_algorithms import QuickUnion, WeightedQuickUnion, WQU_PathCompression
+from random import randint
+import matplotlib.pyplot as plt
 import timeit
+
+num_unions = 5
+range_nodes = 10
 
 def time_test(algorithm):
     t = algorithm(100)
-    t.union(1,2)
-    t.connected(1,2)
+    '''
+    for i in range(num_unions):
+        t.union(randint(0,range_nodes), randint(0,range_nodes))
+    t.connected(randint(0,range_nodes), randint(0,range_nodes))
+    '''
+    t.union(1,4)
+    t.union(2,6)
+    t.union(4,3)
+    t.union(1,7)
+    t.connected(1,3)
     t.union(4,2)
-    t.union(3,4)
     t.connected(0,2)
     t.connected(1,4)
     t.union(0,3)
-    t.connected(0,4)
-
-num_tests = 5
+    t.connected(0,7)
+    
+num_tests = 2
 iterations = 100
-'''
+
+test_iterations = []
+qu_time = []
+wqu_pathcompression_time = []
+
 print('Quick union implementation:')
 for i in range(num_tests):
+    test_iterations.append(iterations)
     t = timeit.timeit(stmt="time_test(QuickUnion)",
             setup="from union_algorithms import QuickUnion; from __main__ import time_test",
             number=iterations)
     print('For %s iterations, it takes %.2f seconds' %(iterations, t))
+    qu_time.append(t)
     iterations = iterations*10
-    
+
     if i == num_tests - 1:
         iterations = 100
 
@@ -36,7 +54,6 @@ for i in range(num_tests):
 
     if i == num_tests - 1:
         iterations = 100
-'''
 
 print('\nWeighted quick union with path compression implementation:')
 for i in range(num_tests):
@@ -44,8 +61,15 @@ for i in range(num_tests):
             setup="from union_algorithms import WQU_PathCompression; from __main__ import time_test",
             number=iterations)
     print('For %s iterations, it takes %.2f seconds' %(iterations, t))
+    wqu_pathcompression_time.append(t)
     iterations = iterations*10
 
     if i == num_tests - 1:
         iterations = 100
+
+plt.plot(test_iterations, qu_time, test_iterations, wqu_pathcompression_time)
+plt.axis([0, max(test_iterations), 0, max(qu_time)])
+plt.ylabel('Time (s)')
+plt.xlabel('Number iterations')
+plt.show()
 
